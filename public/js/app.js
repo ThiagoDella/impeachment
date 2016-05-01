d1d629036c72f1a9c528fa3df722862ecb29ab62
JrLima <juniorlima.e@gmail.com>
2016-04-30 22:22:42 -0300

    regex filters in restrictive order, fixed html syntax errors

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,6 @@ app.controller('mainController',function($scope,$http){
 		url: '/data'
 	}).then(function (res){
 		$scope.deputados = res.data;
-		//console.log($scope.deputados);
 	},function (err){
 		console.log(err);
 	});
@@ -15,35 +14,12 @@ app.controller('mainController',function($scope,$http){
 	/*Filtros com operadores ternario no drop-down:
 		Se TODOS, entao sem restricoes (true), se nao, é comparado UF/Partido do deputado com uf/partido selecionado
 	*/
-	$scope.filterEstado = (x)=> $scope.uf==="all" ? true:x.UF==$scope.uf;
-	$scope.filterPartido = (x)=> $scope.partido==="all" ? true:x.Partido==$scope.partido;
-	$scope.filterVoto = (x)=> $scope.voto==="all" ? true:x.Voto==$scope.voto;
+	$scope.filterUF = (x)=> new RegExp($scope.uf).test(x.UF);
+	$scope.filterPartido = (x)=> new RegExp($scope.partido).test(x.Partido);
+	$scope.filterVoto = (x)=> new RegExp($scope.voto).test(x.Voto);
 	$scope.filterNome = (x)=> new RegExp($scope.nome, "ig").test(x.Nome);
 
 	/*realiza os filtros*/
-	$scope.search = (x) => $scope.filterEstado(x) && $scope.filterPartido(x) && $scope.filterVoto(x) && $scope.filterNome(x);
+	$scope.search = (x) => $scope.filterNome(x) && $scope.filterPartido(x) && $scope.filterUF(x) && $scope.filterVoto(x);
 
-	$scope.getUnique = function(x)
-	{
-	 var u = {}, a = [];
-	 for(var i = 0, l = x.length; i < l; ++i)
-	 {
-	  if(u.hasOwnProperty(x[i])) {continue;}
-	  a.push(x[i]);
-	  u[x[i]] = 1;
-	 }
-	 return a;
-	}
-
-
-/*	$scope.searchValue = function (item){
-		if($scope.search === undefined){
-			return true
-		}else if($scope.search.VotoContra === true){
-			
-			return console.log($scope.deputados.indexOf($scope.deputados.Voto == "A favor")==0);	
-			
-			
-		}
-	}*/
 });
